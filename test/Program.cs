0467b72054134a51b5de9c217ec0195d206cea97
Yusif <yusifyp@code.edu.az>
2022-12-22 19:35:03 +0400

    add dev

--- test/Program.cs
+++ test/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("change");
+            Console.WriteLine("change2");
+            Console.WriteLine("change3");
+
         }
     }
 }
